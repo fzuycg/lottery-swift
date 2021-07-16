127b71add8819173e524da3530632fca2e1a5f41
yangchungui <yangchungui@cibfintech.com>
2021-07-16 16:56:40 +0800

    Initial Commit

---
+++ lottery-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottery-swift
+//
+//  Created by 杨春贵 on 2021/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
