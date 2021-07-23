7ca40850a0907f85808b8049b9368f8bedea678d
yangchungui <yangchungui@cibfintech.com>
2021-07-23 14:48:35 +0800

    add project

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '10.0'
+
+target 'lottery-swift' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for lottery
+
+    pod 'AFNetworking'
+    pod 'SVProgressHUD'
+    pod 'FMDB/SQLCipher'
+    pod 'Masonry'
+    pod 'MJRefresh'
+    pod 'SDWebImage','~> 5.8.0'
+    pod 'DGActivityIndicatorView'
+end
--- lottery-swift/ViewController.swift
+++ lottery-swift/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        self.createUI()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
 
+    func createUI() {
+        let titleLabel = UILabel();
+        titleLabel.frame = self.view.bounds
+        titleLabel.text = "hello"
+        view.addSubview(titleLabel)
+    }
 }
 
